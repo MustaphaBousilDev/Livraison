b746a5db4cb97bd58a32f91a631c0cccf7ef9e01
MustaphaBousilDev <112890579+MustaphaBousilDev@users.noreply.github.com>
2023-10-23 16:39:08 +0100

    add responsive design

--- frontend/src/components/Form/Login.jsx
+++ frontend/src/components/Form/Login.jsx
@@ -1,6 +1,9 @@
 export const Login = () => {
   return (
-    <div className=" absolute z-[10000] w-32 h-32 bg-gray-600 top-[50%] left-[50%] translate-x-[-50%] flex items-center justify-center opacity-70">
+    <div className="
+      absolute z-[10000] w-[95%] sm:w-[80%] md:w-[60%] lg:w-[50%] xl:w-[38%] 2xl:w-[30%] h-[75%] bg-gray-600 top-[50%] 
+      left-[50%] translate-x-[-50%] translate-y-[-50%] flex 
+      items-center justify-center opacity-70 rounded-md lg:rounded-2xl text-white">
         Login
     </div>
   )
