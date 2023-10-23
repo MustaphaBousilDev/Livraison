d9b20b1c730e64154579c4ffb29eacd8eebfd63f
MustaphaBousilDev <112890579+MustaphaBousilDev@users.noreply.github.com>
2023-10-23 16:15:14 +0100

    work in login

---
+++ frontend/src/components/Form/Login.jsx
@@ -0,0 +1,7 @@
+export const Login = () => {
+  return (
+    <div className=" absolute z-[10000] w-32 h-32 bg-gray-600 top-[50%] left-[50%] translate-x-[-50%] flex items-center justify-center opacity-70">
+        Login
+    </div>
+  )
+}
