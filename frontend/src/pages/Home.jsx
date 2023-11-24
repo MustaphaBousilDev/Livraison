56f7e4f7ab8943c7aa70451eaad65b8b9611c5a1
mustaphabousildev <bousilmustapha@gmail.com>
2023-11-24 13:18:48 +0100

    fgfg

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,15 +1,11 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 import { logout } from "../service/api/auth/auth"
-const Home = ( => {
+const Home = ( )=> {
   const [active,setActive] = useState(false)
   
 
-  useEffect(()=>{
-    if(active){
-      logout()
-    }
-  },[active])
+  
   return (
     <button onClick={()=>setActive(true)}>Homed</button>
   )
